6b7e9e339e51c1bc1b296d8a6990841d7ad70c8a
RemiPail <remipailharey@gmail.com>
2020-09-17 14:03:12 +0200

    Edge files

--- src/GraphStructure/ListEdges.java
+++ src/GraphStructure/ListEdges.java
@@ -1,5 +1,37 @@
 package GraphStructure;
 
-public class ListEdges {
+import java.util.ArrayList;
 
+public class ListEdges {
+	private ArrayList<Edge> list;
+	private double value;
+	
+	
+	public void addEdge(Edge e) {
+		this.list.add(e);
+	}
+	
+	public double calcSum(int i) {
+		this.value = 0;
+		for(Edge e : this.list) {
+			this.value += e.getValue(i);
+		}
+		return this.value;
+	}
+	
+	// GETTERS
+	
+	public Edge getEdge(int i) {
+		return list.get(i);
+	}
+	
+	public double getValue() {
+		return this.value;
+	}
+	
+	// SETTERS
+	
+	public void setEdge(int i, Edge e) {
+		this.list.set(i, e);
+	}
 }
