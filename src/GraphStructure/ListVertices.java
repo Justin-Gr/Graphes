5074fd072c7275ddaefa9a519848b80e1b170bef
Epulapp <Epulapp@epulapp2014.univ-lyon1.fr>
2020-09-17 14:03:29 +0200

    implementation Vertex

--- src/GraphStructure/ListVertices.java
+++ src/GraphStructure/ListVertices.java
@@ -1,5 +1,44 @@
 package GraphStructure;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListVertices {
 
+	private List<Vertex> list = new ArrayList<Vertex>();
+	private double value;
+	
+	public ListVertices(List<Vertex> list) {
+		this.list = list;
+		this.value = 0;
+	}
+
+	public void add(Vertex vertex) {
+		this.list.add(vertex);
+	}
+	
+	public Vertex get(int i) {
+		return list.get(i);
+	}
+	
+	public void set(int i, Vertex vertex) {
+		this.list.set(i, vertex);
+	}
+	
+	public double getValue() {
+		return value;
+	}
+	
+	public void setValue(double value) {
+		this.value = value;
+	}
+	
+	public double calcSum(int i) {
+		value = 0;
+		for (Vertex v : list) {
+			value += v.getValue(i);
+		}
+		return value;
+	}
+	
 }
--- src/GraphStructure/Vertex.java
+++ src/GraphStructure/Vertex.java
@@ -1,5 +1,77 @@
 package GraphStructure;
 
 public class Vertex {
-
+	
+	private final int id;
+	private String name;
+	private int degree;
+	private int degreePos;
+	private int degreeNeg;
+	private int color;
+	private final int valuesNb;
+	private double[] values;
+		
+	public Vertex(int id, String name, int valuesNb) {
+		this.id = id;
+		this.name = name;
+		this.valuesNb = valuesNb;
+		this.values = new double[valuesNb];
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getDegree() {
+		return degree;
+	}
+	
+	public void setDegree(int degree) {
+		this.degree = degree;
+	}
+	
+	public int getDegreePos() {
+		return degreePos;
+	}
+	
+	public void setDegreePos(int degreePos) {
+		this.degreePos = degreePos;
+	}
+	
+	public int getDegreeNeg() {
+		return degreeNeg;
+	}
+	
+	public void setDegreeNeg(int degreeNeg) {
+		this.degreeNeg = degreeNeg;
+	}
+	
+	public int getColor() {
+		return color;
+	}
+	
+	public void setColor(int color) {
+		this.color = color;
+	}
+	
+	public int getValuesNb() {
+		return valuesNb;
+	}
+	
+	public double getValue(int i) {
+		return values[i];
+	}
+	
+	public void setValue(int i, double value) {
+		this.values[i] = value;
+	}
+	
 }
